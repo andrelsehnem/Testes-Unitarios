d65265294409198de871f05074c514e64dfd1477
André Luis Sehnem <als10andr@hotmail.com>
2023-03-29 09:52:44 -0300

    Adicionado parte visual do app

---
+++ TesteUnit-App/Agenda.UIDesktop/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Agenda.UIDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContatoNovo = new System.Windows.Forms.Label();
+            this.txtContatoNovo = new System.Windows.Forms.TextBox();
+            this.lblContatoSalvo = new System.Windows.Forms.Label();
+            this.txtContatoSalvo = new System.Windows.Forms.TextBox();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblContatoNovo
+            // 
+            this.lblContatoNovo.AutoSize = true;
+            this.lblContatoNovo.Location = new System.Drawing.Point(12, 9);
+            this.lblContatoNovo.Name = "lblContatoNovo";
+            this.lblContatoNovo.Size = new System.Drawing.Size(82, 15);
+            this.lblContatoNovo.TabIndex = 0;
+            this.lblContatoNovo.Text = "Contato Novo";
+            // 
+            // txtContatoNovo
+            // 
+            this.txtContatoNovo.Location = new System.Drawing.Point(12, 27);
+            this.txtContatoNovo.Name = "txtContatoNovo";
+            this.txtContatoNovo.Size = new System.Drawing.Size(271, 23);
+            this.txtContatoNovo.TabIndex = 1;
+            // 
+            // lblContatoSalvo
+            // 
+            this.lblContatoSalvo.AutoSize = true;
+            this.lblContatoSalvo.Location = new System.Drawing.Point(12, 63);
+            this.lblContatoSalvo.Name = "lblContatoSalvo";
+            this.lblContatoSalvo.Size = new System.Drawing.Size(81, 15);
+            this.lblContatoSalvo.TabIndex = 2;
+            this.lblContatoSalvo.Text = "Contato Salvo";
+            // 
+            // txtContatoSalvo
+            // 
+            this.txtContatoSalvo.Location = new System.Drawing.Point(12, 81);
+            this.txtContatoSalvo.Name = "txtContatoSalvo";
+            this.txtContatoSalvo.Size = new System.Drawing.Size(271, 23);
+            this.txtContatoSalvo.TabIndex = 3;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(208, 127);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btSalvar.TabIndex = 4;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.txtContatoSalvo);
+            this.Controls.Add(this.lblContatoSalvo);
+            this.Controls.Add(this.txtContatoNovo);
+            this.Controls.Add(this.lblContatoNovo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblContatoNovo;
+        private System.Windows.Forms.TextBox txtContatoNovo;
+        private System.Windows.Forms.Label lblContatoSalvo;
+        private System.Windows.Forms.TextBox txtContatoSalvo;
+        private System.Windows.Forms.Button btSalvar;
+    }
+}
---
+++ TesteUnit-App/Agenda.UIDesktop/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Agenda.UIDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            string nome = txtContatoNovo.Text;
+            txtContatoSalvo.Text = nome;
+
+            try
+            {
+                string strCon = @"Server=DSV-ANDRELUIS-5;Database=AgendaT; Trusted_Connection=yes;";
+                //string strCon = @"Data Source=.\sqlexpress;Initial Catalog=AgendaT; Integrated Security=True;";
+                SqlConnection con = new SqlConnection(strCon);
+                con.Open();
+
+                string sql = $@"insert into Contato (Id, Nome) values ({Guid.NewGuid().ToString()}, '{nome}'";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
